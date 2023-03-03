1fedee832fb87c1cf7bf07d181d2d844c6666027
Richard Morris <richmorris55@yahoo.com>
2023-03-03 11:53:43 -0800

    SecondCommit

---
+++ BestBuyBestPractices/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BestBuyBestPractices
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+
+		public void CreateProduct(string name, double price, int categoryID);
+
+	}
+}
+
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -15,6 +15,35 @@ class Program
         string connString = config.GetConnectionString("DefaultConnection");
 
         IDbConnection conn = new MySqlConnection(connString);
+
+
+
+
+        var repo = new DapperProductRepository(conn);
+
+        Console.WriteLine("What is the name of your new product?");
+
+        var prodName = Console.ReadLine();
+
+
+        Console.WriteLine("What is the price?");
+
+        var prodPrice = double.Parse (Console.ReadLine());
+
+        Console.WriteLine("What is the category ID?");
+
+        var prodCat = int.Parse(Console.ReadLine());
+
+
+        repo.CreateProduct(prodName, prodPrice, prodCat);
+
+
+        var prodList = repo.GetAllProducts();
+
+        foreach (var prod in prodList)
+        {
+            Console.WriteLine($"{prod.ProductID} - {prod.Name}");
+        }
     }
 }
 
